785d3205d34dfb42177a02b8980af38b1b48a4d5
sclapple <139606543+sclapple@users.noreply.github.com>
2025-06-16 13:21:12 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -12,5 +12,7 @@ sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/ya
        	goverlay gamescope solaar bluez bluez-utils lib32-libpulse pipewire pipewire-pulse \
        	pipewire-alsa linux-headers xwaylandvideobridge protonplus polkit-gnome gnome-disk-utility \
 	timeshift xorg-xhost uwsm fzf fastanime mpv fish fisher \
- 	nvidia nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia
+ 	nvidia nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia \
+  	aic94xx-firmware ast-firmware linux-firmware-qlogic linux-firmware-bnx2x linux-firmware-liquidio \
+   	linux-firmware-mellanox linux-firmware-nfp wd719x-firmware upd72020x-fw \
        	pamac-all networkmanager greetd nwg-hello egl-wayland && sudo usermod -aG gamemode izaiaha && sudo systemctl enable fstrim.timer && sudo systemctl enable NetworkManager && sudo systemctl enable greetd.service && sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Arch-Hyprland/main/auto-install.sh)
